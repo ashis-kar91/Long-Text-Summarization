ba5bb62841bf514ddd43690bc92bd93135cefba8
Ashis Kar <ashiskar@microsoft.com>
2023-09-15 12:54:22 +0530

    Deleted requirement txt and creating input_files folder if not present

--- API/main.py
+++ API/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile
+import os
 import summarize
 
 app = FastAPI()
@@ -7,7 +8,10 @@ app = FastAPI()
 async def create_upload_file(file: UploadFile):
     try:
         contents = file.file.read()
-        file_path = f'input_files/{file.filename}'
+        input_path = 'input_files'
+        if not os.path.exists(input_path):
+            os.makedirs(input_path)
+        file_path = f'{input_path}/{file.filename}'
         with open(file_path, "wb") as f:
             f.write(contents)
         return summarize.main(file_path)
